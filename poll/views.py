4c9ba014eeacce0074db10f86a06b0889302508e
radek <radek@mbp13.local>
2020-04-16 20:52:35 +0200

    add views: poll_id

--- poll/views.py
+++ poll/views.py
@@ -11,11 +11,11 @@ def create(request):
     return render(request, "poll/create.html", context)
 
 
-def vote(request):
+def vote(request, poll_id):
     context = {}
     return render(request, "poll/vote.html", context)
 
 
-def results(request):
+def results(request, poll_id):
     context = {}
     return render(request, "poll/results.html", context)
